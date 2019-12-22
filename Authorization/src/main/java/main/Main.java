81674e09847ad13da1fa2a499206802a7ac5ebc2
Sergey Tomp <tsv_mail@mail.ru>
2019-12-22 10:31:54 +0300

    Худо-бедно, но заработало из Idea и из .jar с index.html внутри него. Надо разбираться по сути.

--- Authorization/src/main/java/main/Main.java
+++ Authorization/src/main/java/main/Main.java
@@ -6,10 +6,20 @@ import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.resource.Resource;
 import servlets.SessionsServlet;
 import servlets.UsersServlet;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.Enumeration;
 
 /**
  * @author v.chibrikov
@@ -19,21 +29,37 @@ import servlets.UsersServlet;
  *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
  */
 public class Main {
+
     public static void main(String[] args) throws Exception {
+
         AccountService accountService = new AccountService();
 
         accountService.addNewUser(new UserProfile("admin"));
         accountService.addNewUser(new UserProfile("test"));
 
+        ClassLoader cl  = Main.class.getClassLoader();
+        URL f = cl.getResource("public_html/");
+        if (f == null) { throw new RuntimeException("Unable to find resource directory");}
+        URI webRootUri = f.toURI();
+//        URI webRootUri = f.toURI().resolve("./").normalize();
+
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/api/v1/users");
         context.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/api/v1/sessions");
 
-        ResourceHandler resource_handler = new ResourceHandler();
-        resource_handler.setResourceBase("public_html");
+        context.setContextPath("/");
+        context.setBaseResource(Resource.newResource(webRootUri));
+        ServletHolder holderPwd = new ServletHolder("default", DefaultServlet.class);
+        holderPwd.setInitParameter("dirAllowed","true");
+        context.addServlet(holderPwd,"/");
+
+//        ResourceHandler resource_handler = new ResourceHandler();
+//        resource_handler.setResourceBase("Authorization/public_html/");
+//        resource_handler.setResourceBase("/D:/StepicWebServerStudy/Authorization/target/classes/public_html");
 
         HandlerList handlers = new HandlerList();
-        handlers.setHandlers(new Handler[]{resource_handler, context});
+//        handlers.setHandlers(new Handler[]{resource_handler, context});
+        handlers.setHandlers(new Handler[]{context});
 
         Server server = new Server(8080);
         server.setHandler(handlers);
