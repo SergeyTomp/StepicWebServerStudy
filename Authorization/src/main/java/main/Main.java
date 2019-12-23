cc985e7e20d0479e7b519ef44a35c7851fff0cc4
Sergey_Tomp <tsv.091265@gmail.com>
2019-12-23 13:42:36 +0300

    тест Authorization пройден

--- Authorization/src/main/java/main/Main.java
+++ Authorization/src/main/java/main/Main.java
@@ -2,25 +2,19 @@ package main;
 
 import accounts.AccountService;
 import accounts.UserProfile;
-import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.resource.Resource;
 import servlets.SessionsServlet;
+import servlets.SignInServlet;
+import servlets.SignUpServlet;
 import servlets.UsersServlet;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
-import java.util.Enumeration;
-import java.util.logging.Logger;
 
 /**
  * @author v.chibrikov
@@ -47,6 +41,8 @@ public class Main {
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/api/v1/users");
         context.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/api/v1/sessions");
+        context.addServlet(new ServletHolder(new SignUpServlet(accountService)), "/signup");
+        context.addServlet(new ServletHolder(new SignInServlet(accountService)), "/signin");
 
         context.setContextPath("/");
         context.setBaseResource(Resource.newResource(webRootUri));
@@ -66,7 +62,7 @@ public class Main {
         server.setHandler(context);
 
         server.start();
-        Logger.getGlobal().info("Server started");
+        Log.getRootLogger().info("Server started");
         server.join();
     }
 }
